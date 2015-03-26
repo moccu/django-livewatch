e54095b53b9bf584258bbd09092276819a8cda1d
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2015-03-26 11:07:13 +0100

    moved rq task testing to test_tasks

--- livewatch/tests/test_tasks.py
+++ livewatch/tests/test_tasks.py
@@ -3,21 +3,40 @@ from __future__ import absolute_import
 import time
 from datetime import datetime
 
+import mock
 from django.core.cache import cache
 
-from ..tasks import livewatch_update_task
+from ..extensions.rq import livewatch_update_rq_task
+from ..tasks import livewatch_update_celery_task
 
 
-class TestTask:
-
-    def setup(self):
-        self.key = 'livewatch_task'
+class TestRqTask:
+    key = 'livewatch_rq'
 
     def teardown(self):
-        time.sleep(.2)
+        time.sleep(1)
         cache.delete(self.key)
 
-    def test_livewatch_update_task(self):
+    def test_livewatch_update_rq_task(self):
         assert cache.get(self.key) is None
-        livewatch_update_task(self.key)
+        livewatch_update_rq_task(self.key)
+        assert cache.get(self.key) is not None
+
+    @mock.patch('livewatch.extensions.cache.cache.set')
+    def test_livewatch_update_rq_task_cache_not_set(self, cache_mock):
+        assert cache.get(self.key) is None
+        livewatch_update_rq_task(self.key)
+        assert cache.get(self.key) is None
+
+
+class TestCeleryTask:
+    key = 'livewatch_task'
+
+    def teardown(self):
+        time.sleep(1)
+        cache.delete(self.key)
+
+    def test_livewatch_update_celery_task(self):
+        assert cache.get(self.key) is None
+        livewatch_update_celery_task(self.key)
         assert isinstance(cache.get(self.key), datetime) is True
